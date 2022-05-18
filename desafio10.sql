67934e07075a76a089723f3475de42bfe7ec409f
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-05-18 10:12:18 -0300

    fix: adicionado order by por id e alterada a ordem do WHERE e ORDER em req 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
-SELECT * from northwind.purchase_orders ORDER BY created_by DESC WHERE created_by >= 3;
+SELECT * from northwind.purchase_orders WHERE created_by >= 3 ORDER BY created_by DESC, id ASC;
